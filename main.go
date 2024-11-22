0580627f07823832a2b26e63c618bdbab31ad831
Yorn90104 <bingxiao526@gmail.com>
2024-11-22 09:14:46 +0800

    新學的

--- main.go
+++ main.go
@@ -2,290 +2,166 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
+	"time"
 )
 
-var (
-	//歸屬
-	p1 = ""
-	p2 = ""
-	p3 = ""
-	//隨機數
-	ram1 = 0
-	ram2 = 0
-	ram3 = 0
+type LaBaG struct {
+	//遊玩次數
+	Times  int
+	Played int
+
 	//分數
-	score = 0
-	add   = 0
-	//次數
-	times = 30
-	ed    = 0
-	press string
-)
+	Score       int
+	MarginScore int
 
-func test() {
-	fmt.Println("第", ed, "次")
-	fmt.Println(p1, "|", p2, "|", p3)
-	fmt.Println("+", add)
-	fmt.Println("目前分數：", score)
+	//內部判斷變數
+	Rams   [3]int
+	Prizes [3]string
+	//機率與分數字典
+	Rates     [6]int
+	ScoreDict map[string][6]int
 }
 
-func P(ram int) (p string) {
-	//判斷歸屬
-	switch {
-	case ram <= 36:
-		p = "A"
-	case ram <= 60:
-		p = "B"
-	case ram <= 77:
-		p = "C"
-	case ram <= 89:
-		p = "D"
-	case ram <= 97:
-		p = "E"
-	case ram <= 100:
-		p = "F"
+func (Game *LaBaG) result() {
+	Game.Score += Game.MarginScore
+	fmt.Println("第", Game.Played, "次")
+	fmt.Println(Game.Prizes[0], "|", Game.Prizes[1], "|", Game.Prizes[2])
+	fmt.Println("+", Game.MarginScore)
+	fmt.Println("目前分數：", Game.Score)
+	Game.MarginScore = 0
+}
+
+func (Game *LaBaG) GameRunning() bool {
+	if Game.Played < Game.Times {
+		return true
+	} else {
+		return false
 	}
-	return p
 }
 
-func IF() (cond string, typ int) {
-	//判斷情況
-	switch {
-	//三個相同
-	case p1 == p2 && p1 == p3:
-		cond = "3"
-		typ = 0
-	//兩個相同
-	case p1 == p2 || p2 == p3 || p1 == p3:
+func (Game *LaBaG) ramdon_value() {
+	//隨機數
+	rand.Seed(time.Now().UnixNano())
+	Game.Rams[0], Game.Rams[1], Game.Rams[2] = rand.Intn(99)+1, rand.Intn(99)+1, rand.Intn(99)+1
+}
+
+func acc_rate(Rates [6]int) []int {
+	//累加機率成機率區間
+	var result_rate []int
+	acc := 0
+	for _, Rate := range Rates {
+		acc += Rate
+		result_rate = append(result_rate, acc)
+	}
+	return result_rate
+}
+
+func (Game *LaBaG) classify_prize() {
+	//分類
+	rate_range := acc_rate(Game.Rates)
+	fmt.Println("機率區間：", rate_range)
+
+	for i := 0; i < 3; i++ {
 		switch {
-		case p1 == p2:
-			typ = 3
-		case p2 == p3:
-			typ = 1
-		case p1 == p3:
-			typ = 2
+		case Game.Rams[i] <= rate_range[0]:
+			Game.Prizes[i] = "A"
+		case Game.Rams[i] <= rate_range[1]:
+			Game.Prizes[i] = "B"
+		case Game.Rams[i] <= rate_range[2]:
+			Game.Prizes[i] = "C"
+		case Game.Rams[i] <= rate_range[3]:
+			Game.Prizes[i] = "D"
+		case Game.Rams[i] <= rate_range[4]:
+			Game.Prizes[i] = "E"
+		case Game.Rams[i] <= rate_range[5]:
+			Game.Prizes[i] = "F"
 		}
-		cond = "2"
-	//三個不同
-	case p1 != p2 && p2 != p3 && p1 != p3:
-		cond = "1"
-		typ = 0
 	}
-	return cond, typ
 }
 
-func ADD(cond string, typ int) (a int) {
-	//計算分數
-	switch cond {
-	case "3":
-		switch p1 {
-		case "A":
-			a = a + 200
-		case "B":
-			a = a + 600
-		case "C":
-			a = a + 1600
-		case "D":
-			a = a + 1800
-		case "E":
-			a = a + 10000
-		case "F":
-			a = a + 20000
-		}
-	case "2":
-		switch typ {
-		case 3: // 1 & 2
-			switch p1 {
-			case "A":
-				a = a + 100
-			case "B":
-				a = a + 170
-			case "C":
-				a = a + 780
-			case "D":
-				a = a + 870
-			case "E":
-				a = a + 5000
-			case "F":
-				a = a + 10000
-			}
-			switch p3 {
-			case "A":
-				a = a + 30
-			case "B":
-				a = a + 50
-			case "C":
-				a = a + 250
-			case "D":
-				a = a + 290
-			case "E":
-				a = a + 1200
-			case "F":
-				a = a + 2500
-			}
-
-			a = a / 13 * 10
-
-		case 1: // 2 & 3
-			switch p2 {
-			case "A":
-				a = a + 100
-			case "B":
-				a = a + 170
-			case "C":
-				a = a + 780
-			case "D":
-				a = a + 870
-			case "E":
-				a = a + 5000
-			case "F":
-				a = a + 10000
-			}
-			switch p1 {
-			case "A":
-				a = a + 30
-			case "B":
-				a = a + 50
-			case "C":
-				a = a + 250
-			case "D":
-				a = a + 290
-			case "E":
-				a = a + 1200
-			case "F":
-				a = a + 2500
-			}
-
-			a = a / 13 * 10
-
-		case 2: // 1 & 3
-			switch p1 {
-			case "A":
-				a = a + 100
-			case "B":
-				a = a + 170
-			case "C":
-				a = a + 780
-			case "D":
-				a = a + 870
-			case "E":
-				a = a + 5000
-			case "F":
-				a = a + 10000
-			}
-			switch p2 {
-			case "A":
-				a = a + 30
-			case "B":
-				a = a + 50
-			case "C":
-				a = a + 250
-			case "D":
-				a = a + 290
-			case "E":
-				a = a + 1200
-			case "F":
-				a = a + 2500
-			}
-
-			a = a / 13 * 10
-		}
-	case "1":
-		//1
-		switch p1 {
-		case "A":
-			a = a + 30
-		case "B":
-			a = a + 50
-		case "C":
-			a = a + 250
-		case "D":
-			a = a + 290
-		case "E":
-			a = a + 1200
-		case "F":
-			a = a + 2500
-		}
-
-		//2
-		switch p2 {
-		case "A":
-			a = a + 30
-		case "B":
-			a = a + 50
-		case "C":
-			a = a + 250
-		case "D":
-			a = a + 290
-		case "E":
-			a = a + 1200
-		case "F":
-			a = a + 2500
-		}
-
-		//3
-		switch p3 {
-		case "A":
-			a = a + 30
-		case "B":
-			a = a + 50
-		case "C":
-			a = a + 250
-		case "D":
-			a = a + 290
-		case "E":
-			a = a + 1200
-		case "F":
-			a = a + 2500
-		}
-
-		a = a / 3
+func get_score(Prize string, MarginScore int, arr [6]int) int {
+	// 抓取從Array分數
+	switch Prize {
+	case "A":
+		MarginScore += arr[0]
+	case "B":
+		MarginScore += arr[1]
+	case "C":
+		MarginScore += arr[2]
+	case "D":
+		MarginScore += arr[3]
+	case "E":
+		MarginScore += arr[4]
+	case "F":
+		MarginScore += arr[5]
 	}
-	a = int(math.Round(float64(a)))
-	return a
+	return MarginScore
 }
 
-func Score(a int, s int) (scr int) {
-	scr = s + a
-	a = 0
-	return scr
-}
+func (Game *LaBaG) calculate_score() {
+	switch {
+	case Game.Prizes[0] == Game.Prizes[1] && Game.Prizes[1] == Game.Prizes[2]: //三個相同
+		Game.MarginScore = get_score(Game.Prizes[0], Game.MarginScore, Game.ScoreDict["same3"])
 
-func END(s int) {
-	fmt.Println("\n遊戲結束，總分為：", s)
+	case Game.Prizes[0] == Game.Prizes[1] || Game.Prizes[1] == Game.Prizes[2] || Game.Prizes[0] == Game.Prizes[2]: //兩個相同
+		switch {
+		case Game.Prizes[0] == Game.Prizes[1]:
+			Game.MarginScore = get_score(Game.Prizes[0], Game.MarginScore, Game.ScoreDict["same2"])
+			Game.MarginScore = get_score(Game.Prizes[2], Game.MarginScore, Game.ScoreDict["same1"])
+		case Game.Prizes[1] == Game.Prizes[2]:
+			Game.MarginScore = get_score(Game.Prizes[1], Game.MarginScore, Game.ScoreDict["same2"])
+			Game.MarginScore = get_score(Game.Prizes[0], Game.MarginScore, Game.ScoreDict["same1"])
+		case Game.Prizes[2] == Game.Prizes[0]:
+			Game.MarginScore = get_score(Game.Prizes[2], Game.MarginScore, Game.ScoreDict["same2"])
+			Game.MarginScore = get_score(Game.Prizes[1], Game.MarginScore, Game.ScoreDict["same1"])
+		}
+		Game.MarginScore = Game.MarginScore / 13 * 10
+
+	case Game.Prizes[0] != Game.Prizes[1] && Game.Prizes[1] != Game.Prizes[2] && Game.Prizes[0] != Game.Prizes[2]: //三個皆不同
+		Game.MarginScore = get_score(Game.Prizes[0], Game.MarginScore, Game.ScoreDict["same1"])
+		Game.MarginScore = get_score(Game.Prizes[1], Game.MarginScore, Game.ScoreDict["same1"])
+		Game.MarginScore = get_score(Game.Prizes[2], Game.MarginScore, Game.ScoreDict["same1"])
+
+		Game.MarginScore /= 3
+	}
 }
 
 func main() {
-	for ed < times {
 
-		press = ""
+	Game := LaBaG{
+		Times:       30,
+		Played:      0,
+		Score:       0,
+		MarginScore: 0,
+		Rams:        [3]int{0, 0, 0},
+		Prizes:      [3]string{"", "", ""},
+		Rates:       [6]int{36, 24, 17, 12, 8, 3},
+		ScoreDict: map[string][6]int{
+			"same3": {625, 1250, 2100, 2500, 10000, 20000},
+			"same2": {350, 650, 1080, 1250, 5000, 10000},
+			"same1": {150, 220, 380, 420, 1250, 2500},
+		},
+	}
+
+	for Game.GameRunning() {
+		var press string
 		fmt.Println("請按ENTER")
 		fmt.Scanln(&press)
 
 		if press == "" {
-			ram1 = rand.Intn(99) + 1
-			ram2 = rand.Intn(99) + 1
-			ram3 = rand.Intn(99) + 1
+			Game.ramdon_value()
 
-			p1 = P(ram1)
-			p2 = P(ram2)
-			p3 = P(ram3)
+			Game.classify_prize()
+			Game.calculate_score()
 
-			cond, typ := IF()
-
-			add = ADD(cond, typ)
-
-			score = Score(add, score)
-			ed = ed + 1
-
-			test()
-
+			Game.Played++
+			Game.result()
 		} else {
 			fmt.Println("請按ENTER")
 		}
+
 	}
 
-	END(score)
+	fmt.Printf("\n遊戲結束！\n最終分數為：%d", Game.Score)
 }
